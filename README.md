165c793d04968e9be9c9dd596bc806eb69371e9d
mickelindahl <mikael.lindahl.07@gmail.com>
2017-11-05 12:35:55 +0000

    backup script

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 # Backup
 
-# Docker wordpress
-
-# Backup
+## Manual
 
 Run
 ```
@@ -13,4 +11,20 @@ sudo ./backup.sh {container-id} {db-name} {'db-user} {db-pass}
 
 Done!
 
+## Cron
+
+Setup a cronjob that backups day 1,7,14,21, and 28 of the month
+
+Run 
+```
+sudo su
+crontab -e
+```
+Pase
+```
+00 01 1,7,14,21,28 * * {source-root}/backup.sh {container-id} {db-name} {'db-user} {db-pass} `date +%d`
+```
+Save
+
+Done!
 
--- backup.sh
+++ backup.sh
@@ -1,23 +1,41 @@
 #:/bash/bin
 
 # Ecample:
-# backup.sh container-id database-name database-user database-password 
+# backup.sh container-id database-name database-user database-password
+# or
+# backup.sh container-id database-name database-user database-password suffix
 
 CONTAINER_ID=$1
 DB_NAME=$2
 DB_USER=$3
 DB_PASS=$4
 
-DATE=`date +%y%m%d`
-BACKUP_DIR="backups/backup-"${DATE} 
+if [ -z "$5" ]; then
 
+   SUFFIX=`date +%y%m%d`
+
+
+else
+
+   SUFFIX=$5
+
+fi
+
+echo "Enter script dir"
+cd $(dirname $0)
+echo $(pwd)
+
+BACKUP_DIR=$(pwd)/"backups/backup-"${SUFFIX}
+
+#echo $SCRIPT_DIR
 
 if [ -d "$BACKUP_DIR" ]; then
 
-  echo ${BACKUP_DIR}" found, rremoving"
+  echo ${BACKUP_DIR}" found, removing"
   rm -r ${BACKUP_DIR}
 
 fi
+
 
 echo "Create directory "${BACKUP_DIR}
 mkdir -p backups
@@ -25,6 +43,15 @@ mkdir -p backups
 echo "Copy html"
 mkdir -p ${BACKUP_DIR}/html
 cp -R html ${BACKUP_DIR}/html
+
+echo "Create html.tar.gz"
+cd ${BACKUP_DIR}
+tar -zcvf html.tar.gz html
+cd ..
+cd ..
+
+echo "Remove html folder"
+rm -r ${BACKUP_DIR}/html
 
 echo "Backup mysql from container"
 docker exec ${CONTAINER_ID} /usr/bin/mysqldump -u ${DB_USER} --password=${DB_PASS} ${DB_NAME} > ${BACKUP_DIR}/backup.sql
